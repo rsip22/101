6a862c1eb7edba473c856e657431a52ef6f2d99f
rsip22 <rsip22@openmailbox.org>
2018-06-23 02:34:51 -0300

    Day 4: TDD with Django. Some more functional tests

--- 100daysofcode/Python/testing_goat/functional_tests.py
+++ 100daysofcode/Python/testing_goat/functional_tests.py
@@ -3,8 +3,9 @@ Functional tests for the to-do Django app
 
 """
 import unittest
+import time
 from selenium import webdriver
-
+from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(unittest.TestCase):
     """
@@ -23,15 +24,30 @@ class NewVisitorTest(unittest.TestCase):
 
         # She sees the page title and header mention to-do lists
         self.assertIn('To-do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-do', header_text)
 
         # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # She types "Buy peacock feathers" into a text box (Anna's hobby
         # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Anna is very methodical)
@@ -43,7 +59,7 @@ class NewVisitorTest(unittest.TestCase):
         # explanatory text to that effect.
 
         # She visits that URL - her to-do list is still there.
-
+        self.fail('Finish the test!')
         # Satisfied, she goes back to sleep
 
 
