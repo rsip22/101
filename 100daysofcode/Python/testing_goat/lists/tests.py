f0e5d6b0a121ae71dc141d6a206347acc86d4e63
rsip22 <rsip22@openmailbox.org>
2018-06-24 01:24:10 -0300

    Day 5: Refactor home page view to use a template

--- 100daysofcode/Python/testing_goat/lists/tests.py
+++ 100daysofcode/Python/testing_goat/lists/tests.py
@@ -8,16 +8,6 @@ from lists.views import home_page
 # Create your tests here.
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_homepage_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
     def test_homepage_returns_correct_html(self):
         response = self.client.get('/')
-        html = response.content.decode('utf8')
-        expected_html = render_to_string('home.html')
-        self.assertEqual(html, expected_html)
-        self.assertTrue(html.startswith('<!DOCTYPE html>'))
-        self.assertIn('<title>To-do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
-        self.assertTemplateUsed(response, 'wrong.html')
+        self.assertTemplateUsed(response, 'home.html')
