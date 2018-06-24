b5f0217fe1bac97e6881a4acb90104eac08c08c4
rsip22 <rsip22@openmailbox.org>
2018-06-24 01:15:45 -0300

    Day 5: breaking the template test

--- 100daysofcode/Python/testing_goat/lists/tests.py
+++ 100daysofcode/Python/testing_goat/lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
@@ -12,9 +13,11 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_homepage_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
+        response = self.client.get('/')
         html = response.content.decode('utf8')
+        expected_html = render_to_string('home.html')
+        self.assertEqual(html, expected_html)
         self.assertTrue(html.startswith('<!DOCTYPE html>'))
         self.assertIn('<title>To-do lists</title>', html)
         self.assertTrue(html.strip().endswith('</html>'))
+        self.assertTemplateUsed(response, 'wrong.html')
