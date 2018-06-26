0e8e9ec2c728937419e34a40fa5b4c48ba3812b2
rsip22 <rsip22@openmailbox.org>
2018-06-26 17:37:44 -0300

    Day 6: TDD. Checking for new itens added

--- 100daysofcode/Python/testing_goat/functional_tests.py
+++ 100daysofcode/Python/testing_goat/functional_tests.py
@@ -46,15 +46,29 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            f"New to-do item did not appear in table. Contents were: \n{table.text}"
+        self.assertIn(
+            '1: Buy peacock feathers',
+            [row.text for row in rows]
         )
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Anna is very methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(
+            '1: Buy peacock feathers',
+            [row.text for row in rows]
+        )
+        self.assertIn(
+            '2: Buy peacock feathers to make a fly',
+            [row.text for row in rows]
+        )
 
         # Anna wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
