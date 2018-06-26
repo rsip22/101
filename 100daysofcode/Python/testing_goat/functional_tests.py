627e81a22746cd56c3d4069cf0f8743a5c82ac09
rsip22 <rsip22@openmailbox.org>
2018-06-26 17:19:56 -0300

    Day 6: TDD working on templates and context

--- 100daysofcode/Python/testing_goat/functional_tests.py
+++ 100daysofcode/Python/testing_goat/functional_tests.py
@@ -48,7 +48,7 @@ class NewVisitorTest(unittest.TestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
             any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
+            f"New to-do item did not appear in table. Contents were: \n{table.text}"
         )
 
         # There is still a text box inviting her to add another item. She
--- 100daysofcode/Python/testing_goat/lists/tests.py
+++ 100daysofcode/Python/testing_goat/lists/tests.py
@@ -8,6 +8,11 @@ from lists.views import home_page
 # Create your tests here.
 class HomePageTest(TestCase):
 
-    def test_homepage_returns_correct_html(self):
+    def test_uses_home_template(self):
         response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
