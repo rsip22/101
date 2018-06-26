e12b29d7335eb5ad74682e8a4a55a3019fe5ef5b
rsip22 <rsip22@openmailbox.org>
2018-06-26 14:40:21 -0300

    Day 7: Still TDD, going back to the right failure

--- 100daysofcode/Python/testing_goat/functional_tests.py
+++ 100daysofcode/Python/testing_goat/functional_tests.py
@@ -7,6 +7,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+
 class NewVisitorTest(unittest.TestCase):
     """
     Functional tests for a new visitor
@@ -41,16 +42,10 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(10)
+        time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
-        print('========================')
-        print('table:', table)
-        print('========================')
         rows = table.find_elements_by_tag_name('tr')
-        print('========================')
-        print('rows:', rows)
-        print('========================')
         self.assertTrue(
             any(row.text == '1: Buy peacock feathers' for row in rows),
             "New to-do item did not appear in table"
